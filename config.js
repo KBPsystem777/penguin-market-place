022edfd899e00e3f209060dc0c49975965b572db
koleen.bp <koleen.bp@outlook.com>
2022-01-30 01:17:28 +0800

    Initial commit to Penguin-NFT-Marketplace

---
+++ config.js
@@ -0,0 +1,11 @@
+export const nftaddress = "0xcce12f166C3e0C3638153CD50a068Eab5388Bc2c"
+export const nftmarketaddress = "0x97abCe54c689f3aCA92425FdAe79ac11a32e1B81"
+//EDIT !!!
+let x = 1
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["ipfs.infura.io"],
+  },
+  env: {
+    rpcEndpoint:
+      "https://ropsten.infura.io/v3/9a81821557584253bc1f28ed3aa24634",
+    infuraUrl: "https://ropsten.infura.io/v3/9a81821557584253bc1f28ed3aa24634",
+    infuraId: "9a81821557584253bc1f28ed3aa24634",
+  },
+}
